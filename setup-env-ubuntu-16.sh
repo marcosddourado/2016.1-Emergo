25139f5717fdc721a22b8c3530dc3d1d0284abcb
Marcos Dourado <marcosdourado92@gmail.com>
2016-11-28 22:46:48 -0200

    updates setup script

--- setup-env-ubuntu-16.sh
+++ setup-env-ubuntu-16.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 echo "Ubuntu 16.04"
@@ -6,21 +7,38 @@ sleep 1
 
 echo "Enter your password"
 sudo apt-get update
-sudo add-apt-repository ppa:webupd8team/java
+echo "Checking if java is installed"
+pacote=$(dpkg --get-selections | grep oracle-java8-set-default)
+sleep 2
+if [ -n "$pacote" ] ;
+then echo
+     echo "Package already installed"
+else echo
+     sudo add-apt-repository ppa:webupd8team/java
+     echo "Setting up the J environment"
+     sudo apt install oracle-java8-installer
+     sudo update-alternatives --config java
+     echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" >> ~/.bashrc
+     source ~/.bashrc
+     echo "JAVA_HOME: $JAVA_HOME"
+fi
 
-echo "Setting up the J environment"
-sudo apt install oracle-java8-installer
-sudo update-alternatives --config java
-echo "export JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" >> ~/.bashrc
-source ~/.bashrc
-echo "JAVA_HOME: $JAVA_HOME"
+echo "Checking if android studio is installed"
+pacote=$(dpkg --get-selections | grep android-studio)
+sleep 2
+if [ -n "$pacote" ] ;
+then echo
+     echo "Package already installed"
+else echo
+     
+  echo "Downloading Android Studio"
+  echo "Setting up android environment"
+  sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+  sudo add-apt-repository ppa:paolorotolo/android-studio
+  sudo apt-get update
+  sudo apt-get install oracle-java8-installer 
+  sudo apt-get install android-studio
+  echo "Creating shortcut to desktop"
+  echo -e "[Desktop Entry]\nVersion=1.0\nType=Application\nName=Android Studio\nExec='/opt/android-studio/bin/studio.sh' %f\nIcon=/opt/android-studio/bin/studio.png\nCategories=Development;IDE;\nTerminal=false\nStartupNotify=true\nStartupWMClass=android-studio" >> ~/.local/share/applications/androidstudio.desktop
 
-echo "Setting up android environment"
-cd 
-sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
-cd ~/Downloads/
-echo "Downloading Android Studio"
-wget https://dl.google.com/dl/android/studio/ide-zips/2.2.1.0/android-studio-ide-145.3330264-linux.zip?hl=pt-br
-sudo apt-get install unzip
-unzip android-studio-ide-145.3330264-linux.zip?hl=pt-br
-./android-studio/bin/studio.sh
+fi
